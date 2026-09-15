5bb913264f7a23d591b9bf80c5537375829f1f76
trabelsimo <trabelsi.mohamedamine.1@esprit.tn>
2026-09-15 09:51:33 +0100

    commit

--- src/main/java/com/gpm/finance/repository/OtExterneAutreResponsableRepository.java
+++ src/main/java/com/gpm/finance/repository/OtExterneAutreResponsableRepository.java
@@ -4,9 +4,16 @@ import com.gpm.finance.domain.OtExterneAutreResponsable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Spring Data SQL repository for the OtExterneAutreResponsable entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface OtExterneAutreResponsableRepository extends JpaRepository<OtExterneAutreResponsable, Long> {}
+public interface OtExterneAutreResponsableRepository extends JpaRepository<OtExterneAutreResponsable, Long> {
+
+    List<OtExterneAutreResponsable> findByOtExterneId(Long otExterneId);
+
+    void deleteByOtExterneId(Long otExterneId);
+}
